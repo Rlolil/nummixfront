b7f62d417dbd8236962f96a89b939390cae36b30
sananasgarov <prog58816@gmail.com>
2025-09-25 22:00:13 +0400

    Second

---
+++ src/pages/sidebar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SidebarComp from '../../components/sidebar'
+
+function Sidebar() {
+  return (
+    <div>
+        <SidebarComp />
+    </div>
+  )
+}
+
+export default Sidebar
