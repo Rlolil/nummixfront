df598a71d2ada4c54da2a1a1f4a49f1d17e387ce
orujaliev <oruceliyevseccat3@gmail.com>
2025-09-28 21:02:31 +0400

    payment trans

---
+++ src/pages/payments/cards/index.jsx
@@ -0,0 +1,19 @@
+export default function Cards({ title, icons, numberdes, description }) {
+    return (
+
+        <>
+            <div className="card card-border bg-base-100 border-zinc-300 shadow-md">
+                <div className="card-body">
+                    <div className="flex items-center justify-between">
+                        <h2 className="card-title">{title}</h2>
+                        <span>{icons}</span>
+                    </div>
+                    <article className="space-y-2">
+                        <p className="font-bold">{numberdes}</p>
+                        <p>{description}</p>
+                    </article>
+                </div>
+            </div>
+        </>
+    );
+}
