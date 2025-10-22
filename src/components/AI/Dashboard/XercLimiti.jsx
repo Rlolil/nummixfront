1f9063efb8cccb6078485173339d18d5e4fe30c2
sananasgarov <prog58816@gmail.com>
2025-10-23 00:09:59 +0400

    error fix

--- src/components/AI/Dashboard/XercLimiti.jsx
+++ src/components/AI/Dashboard/XercLimiti.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-const xercLimiti = () => {
+function XercLimiti() {
   const { t } = useTranslation();
   const cariXerc = 67890;
   const limit = 80000;
@@ -25,6 +25,6 @@ const xercLimiti = () => {
       </div>
     </div>
   );
-};
+}
 
-export default xercLimiti;
+export default XercLimiti;
