6f0ca5e51d1e80405f29d06ab32be8fbb46b2545
Insun03 <muradsuleymanzade1@gmail.com>
2025-10-27 22:38:43 +0400

    all deleted

--- src/pages/salescustomers/components/ShowMore/SalesHistory.jsx
+++
@@ -1,6 +0,0 @@
-import { useTranslation } from "react-i18next";
-
-export default function SalesHistory() {
-    const { t } = useTranslation();
-    return <div>{t("pages.sales.customers.tabs.salesHistory")}</div>;
-}
