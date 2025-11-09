ec130ccb87f1bf9cd89c85ff99dd186bf30ff249
sananasgarov <prog58816@gmail.com>
2025-11-09 22:31:18 +0400

    Fix: Set initial state of AiMini modal to closed

--- src/pages/aimini/index.jsx
+++ src/pages/aimini/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Modal from "../../components/AI/Ai-Assistent/Modal";
 
 function AiMini() {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
     function onClose() {
         setIsOpen(false);
     }
